63f72b545ca1ed08e09bf9b90529e83f9e0a8c61
Sanjay Verma <sanjay.verma@ranosys.com>
2019-04-19 14:41:58 +0530

    added onchange handler for get selected values

--- src/Lib/Multiselect.js
+++ src/Lib/Multiselect.js
@@ -6,110 +6,114 @@ import { createTree } from "./createTree";
 import { updateChildState, findInTree, updateTreeState } from "./helper";
 
 class Multiselect extends React.Component {
-  state = {
-    isDropdownOpen: false,
-    search: "",
-    selectedState: {}
-  };
-
-  reCreateTreeState = (checked, d) => {
-    const { selectedState } = this.state;
-    const { data } = this.props;
-
-    const searchPath = data.reduce((acc, child) => {
-      const findResult = findInTree(parseInt(d.id), child);
-      if (!_.isEmpty(findResult, true)) {
-        return findResult;
-      }
-      return acc;
-    }, undefined);
-
-    if (searchPath && searchPath.path && searchPath.path instanceof Array) {
-      searchPath.path.pop();
-    }
-
-    selectedState[parseInt(d.id)] = {
-      status: checked.toString(),
-      ...d
+    state = {
+        isDropdownOpen: false,
+        search: "",
+        selectedState: {}
     };
 
-    let updatedSelectedState = selectedState;
-    if (d.child) {
-      updatedSelectedState = updateChildState(
-        selectedState,
-        d.child,
-        checked.toString()
-      );
-    }
+    reCreateTreeState = (checked, d) => {
+        const { selectedState } = this.state;
+        const { data } = this.props;
 
-    const newSelectedState = searchPath
-      ? updateTreeState(searchPath, updatedSelectedState)
-      : updatedSelectedState;
-
-    this.setState({
-      selectedState: newSelectedState
-    });
-  };
-
-  onChange = (e, d) => {
-    const { checked } = e.target;
-    this.reCreateTreeState(checked, d);
-  };
-
-  render() {
-    const { search, selectedState } = this.state;
-    const { data } = this.props;
-
-    const dropDownItems = createTree(
-      data,
-      search,
-      selectedState,
-      this.onChange
-    );
-    return (
-      <React.Fragment>
-        <div className="instructions">
-          (Click to expand and select states to filter)
-        </div>
-        <div className="dropdown-container">
-          <div
-            className="dropdown-button noselect"
-            onClick={() =>
-              this.setState(({ isDropdownOpen }) => {
-                return {
-                  isDropdownOpen: !isDropdownOpen
-                };
-              })
+        const searchPath = data.reduce((acc, child) => {
+            const findResult = findInTree(parseInt(d.id), child);
+            if (!_.isEmpty(findResult, true)) {
+                return findResult;
             }
-          >
-            <TagInput
-              onChange={e => this.setState({ search: e.target.value })}
-              selected={this.state.selectedState}
-              onItemRemove={({ status, ...item }) =>
-                this.reCreateTreeState(false, item)
-              }
-            />
-            <i className="fa fa-filter" />
-          </div>
-          <div
-            className={`dropdown-list ${
-              this.state.isDropdownOpen ? "show" : "hide"
-            }`}
-          >
-            {dropDownItems}
-          </div>
+            return acc;
+        }, undefined);
+
+        if (searchPath && searchPath.path && searchPath.path instanceof Array) {
+            searchPath.path.pop();
+        }
+
+        selectedState[parseInt(d.id)] = {
+            status: checked.toString(),
+            ...d
+        };
+
+        let updatedSelectedState = selectedState;
+        if (d.child) {
+            updatedSelectedState = updateChildState(
+                selectedState,
+                d.child,
+                checked.toString()
+            );
+        }
+
+        const newSelectedState = searchPath
+            ? updateTreeState(searchPath, updatedSelectedState)
+            : updatedSelectedState;
+
+        this.setState({
+            selectedState: newSelectedState
+        }, () => {
+            this.props.onChange(this.state.selectedState);
+        });
+    };
+
+    onChange = (e, d) => {
+        const { checked } = e.target;
+        this.reCreateTreeState(checked, d);
+    };
+
+    render() {
+        const { search, selectedState } = this.state;
+        const { data } = this.props;
+
+        const dropDownItems = createTree(
+            data,
+            search,
+            selectedState,
+            this.onChange
+        );
+        return (
+            <React.Fragment>
+                <div className="instructions">
+                    (Click to expand and select states to filter)
         </div>
-      </React.Fragment>
-    );
-  }
+                <div className="dropdown-container">
+                    <div
+                        className="dropdown-button noselect"
+                        onClick={() =>
+                            this.setState(({ isDropdownOpen }) => {
+                                return {
+                                    isDropdownOpen: !isDropdownOpen
+                                };
+                            })
+                        }
+                    >
+                        <TagInput
+                            onChange={e => this.setState({ search: e.target.value })}
+                            selected={this.state.selectedState}
+                            onItemRemove={({ status, ...item }) =>
+                                this.reCreateTreeState(false, item)
+                            }
+                        />
+                        <i className="fa fa-filter" />
+                    </div>
+                    <div
+                        className={`dropdown-list ${
+                            this.state.isDropdownOpen ? "show" : "hide"
+                            }`}
+                    >
+                        {dropDownItems}
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
 Multiselect.propTypes = {
-  data: PropTypes.array
+    data: PropTypes.array,
+    onChange: PropTypes.func
 };
 
 Multiselect.defaultProps = {
-  data: []
+    data: [],
+    onChange: () => { }
 };
 
 export default Multiselect;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,61 +1,66 @@
 import React from "react";
 import Multiselect from "../Lib/Multiselect";
 
+const getSelected = (selected) => {
+    console.log('your selected value is => ', selected);
+}
+
 const Home = () => {
-  const sampleData = [
-    {
-      id: 1,
-      title: "First",
-      child: [
+    const sampleData = [
         {
-          id: 2,
-          title: "First.1",
-          child: [
-            {
-              id: 3,
-              title: "Mayank"
-            }
-          ]
-        }
-      ]
-    },
-    {
-      id: 4,
-      title: "Second",
-      child: [
-        {
-          id: 5,
-          title: "Second.1",
-          child: [
-            {
-              id: 6,
-              title: "Second.1.1"
-            }
-          ]
+            id: 1,
+            title: "First",
+            child: [
+                {
+                    id: 2,
+                    title: "First.1",
+                    child: [
+                        {
+                            id: 3,
+                            title: "Mayank"
+                        }
+                    ]
+                }
+            ]
         },
         {
-          id: 7,
-          title: "Second.2",
-          child: [
-            {
-              id: 8,
-              title: "Second.2.1"
-            },
-            {
-              id: 9,
-              title: "Second.2.2"
-            }
-          ]
+            id: 4,
+            title: "Second",
+            child: [
+                {
+                    id: 5,
+                    title: "Second.1",
+                    child: [
+                        {
+                            id: 6,
+                            title: "Second.1.1"
+                        }
+                    ]
+                },
+                {
+                    id: 7,
+                    title: "Second.2",
+                    child: [
+                        {
+                            id: 8,
+                            title: "Second.2.1"
+                        },
+                        {
+                            id: 9,
+                            title: "Second.2.2"
+                        }
+                    ]
+                }
+            ]
         }
-      ]
-    }
-  ];
-  return (
-    <React.Fragment>
-      <h1>Home page</h1>
-      <Multiselect data={sampleData} />
-    </React.Fragment>
-  );
+    ];
+
+    return (
+        <React.Fragment>
+            <h1>Home page</h1>
+            <Multiselect data={sampleData} onChange={getSelected} />
+        </React.Fragment>
+    );
 };
 
 export default Home;
