c9a57f78a3dcd7dd1990039d97bd562ac26a2de3
Mayank Saxena <mayank.saxena@dporganizer.com>
2019-04-22 12:39:13 +0530

    Dropdown will open on Focus and will be closed on blur

--- src/Lib/Multiselect.js
+++ src/Lib/Multiselect.js
@@ -1,145 +1,163 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from 'styled-components';
+import styled from "styled-components";
 import _ from "lodash";
 import TagInput from "./TagInput";
 import { createTree } from "./createTree";
 import { updateChildState, findInTree, updateTreeState } from "./helper";
 
 const Dropdown = styled.div`
-    width: 400px;
-    border: 1px solid lightgray;
-    border-radius: 5px;
-    font-size: 14px;
-    font-family: sans-serif;
-    overflow: auto;
-    position: relative;
+  width: 400px;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  font-size: 14px;
+  font-family: sans-serif;
+  overflow: auto;
+  position: relative;
+  margin-left: 10px;
 
-    .dropdown-inner {
-        float: left;
-        width: 100%;
-        border-bottom: 1px solid lightgray;
-        box-sizing: border-box;
-        padding: 0;
-        margin: 0;
-        border-radius: 5px 5px 0px 0px;
-        background: transparent;
+  .dropdown-inner {
+    float: left;
+    width: 100%;
+    border-bottom: 1px solid lightgray;
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    border-radius: 5px 5px 0px 0px;
+    background: transparent;
 
-        background-image:linear-gradient(45deg, transparent 50%, gray 50%),
-        linear-gradient(135deg, gray 50%, transparent 50%),
-        linear-gradient(to right, #ccc, #ccc);
-        background-position: calc(100% - 20px) calc(1em + -2px),calc(100% - 15px) calc(1em + -2px),calc(100% - 2.5em) 0.3em;
-        background-size: 6px 7px,5px 6px,1px 1.5em;
-        background-repeat: no-repeat;
-    }
+    background-image: linear-gradient(45deg, transparent 50%, gray 50%),
+      linear-gradient(135deg, gray 50%, transparent 50%),
+      linear-gradient(to right, #ccc, #ccc);
+    background-position: calc(100% - 20px) calc(1em + -2px),
+      calc(100% - 15px) calc(1em + -2px), calc(100% - 2.5em) 0.3em;
+    background-size: 6px 7px, 5px 6px, 1px 1.5em;
+    background-repeat: no-repeat;
+  }
 `;
 
 class Multiselect extends React.Component {
-    state = {
-        isDropdownOpen: false,
-        search: "",
-        selectedState: {}
-    };
+  state = {
+    isDropdownOpen: false,
+    search: "",
+    selectedState: {}
+  };
 
-    reCreateTreeState = (checked, d) => {
-        const { selectedState } = this.state;
-        const { data } = this.props;
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
 
-        const searchPath = data.reduce((acc, child) => {
-            const findResult = findInTree(parseInt(d.id), child);
-            if (!_.isEmpty(findResult, true)) {
-                return findResult;
-            }
-            return acc;
-        }, undefined);
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
 
-        if (searchPath && searchPath.path && searchPath.path instanceof Array) {
-            searchPath.path.pop();
-        }
+  setWrapperRef = node => {
+    this.wrapperRef = node;
+  };
 
-        selectedState[parseInt(d.id)] = {
-            status: checked.toString(),
-            ...d
-        };
-
-        let updatedSelectedState = selectedState;
-        if (d.child) {
-            updatedSelectedState = updateChildState(
-                selectedState,
-                d.child,
-                checked.toString()
-            );
-        }
-
-        const newSelectedState = searchPath
-            ? updateTreeState(searchPath, updatedSelectedState)
-            : updatedSelectedState;
-
-        this.setState({
-            selectedState: newSelectedState
-        }, () => {
-            this.props.onChange(this.state.selectedState);
-        });
-    };
-
-    onChange = (e, d) => {
-        const { checked } = e.target;
-        this.reCreateTreeState(checked, d);
-    };
-
-    render() {
-        const { search, selectedState } = this.state;
-        const { data } = this.props;
-        return (
-            <React.Fragment>
-                <div className="instructions">
-                    (Click to expand and select states to filter)
-                </div>
-                <Dropdown >
-                    <div className="dropdown-inner dropdown-button noselect"
-                        onClick={() =>
-                            this.setState(({ isDropdownOpen }) => {
-                                return {
-                                    isDropdownOpen: !isDropdownOpen
-                                };
-                            })
-                        }
-                    >
-                        <TagInput
-                            onChange={e => this.setState({ search: e.target.value })}
-                            selected={this.state.selectedState}
-                            onItemRemove={({ status, ...item }) =>
-                                this.reCreateTreeState(false, item)
-                            }
-                        />
-                        <i className="fa fa-filter" />
-                    </div>
-                    <div
-                        className={`${
-                            this.state.isDropdownOpen ? "show" : "hide"
-                            }`}
-                    >
-                        {createTree(
-                            data,
-                            search,
-                            selectedState,
-                            this.onChange
-                        )}
-                    </div>
-                </Dropdown>
-            </React.Fragment>
-        );
+  handleClickOutside = e => {
+    if (this.wrapperRef && !this.wrapperRef.contains(e.target)) {
+      this.toggleDropdown(false);
     }
+  };
+
+  reCreateTreeState = (checked, d) => {
+    const { selectedState } = this.state;
+    const { data } = this.props;
+
+    const searchPath = data.reduce((acc, child) => {
+      const findResult = findInTree(parseInt(d.id), child);
+      if (!_.isEmpty(findResult, true)) {
+        return findResult;
+      }
+      return acc;
+    }, undefined);
+
+    if (searchPath && searchPath.path && searchPath.path instanceof Array) {
+      searchPath.path.pop();
+    }
+
+    selectedState[parseInt(d.id)] = {
+      status: checked.toString(),
+      ...d
+    };
+
+    let updatedSelectedState = selectedState;
+    if (d.child) {
+      updatedSelectedState = updateChildState(
+        selectedState,
+        d.child,
+        checked.toString()
+      );
+    }
+
+    const newSelectedState = searchPath
+      ? updateTreeState(searchPath, updatedSelectedState)
+      : updatedSelectedState;
+
+    this.setState(
+      {
+        selectedState: newSelectedState
+      },
+      () => {
+        this.props.onChange(this.state.selectedState);
+      }
+    );
+  };
+
+  onChange = (e, d) => {
+    const { checked } = e.target;
+    this.reCreateTreeState(checked, d);
+  };
+
+  toggleDropdown = status => {
+    this.setState(({ isDropdownOpen }) => {
+      return {
+        isDropdownOpen: status || !isDropdownOpen
+      };
+    });
+  };
+
+  render() {
+    const { search, selectedState } = this.state;
+    const { data } = this.props;
+    return (
+      <React.Fragment>
+        <div className="instructions">
+          (Click to expand and select states to filter)
+        </div>
+        <Dropdown ref={this.setWrapperRef}>
+          <div className="dropdown-inner dropdown-button noselect">
+            <TagInput
+              onChange={e => this.setState({ search: e.target.value })}
+              selected={this.state.selectedState}
+              onItemRemove={({ status, ...item }) =>
+                this.reCreateTreeState(false, item)
+              }
+              toggleDropdown={this.toggleDropdown}
+            />
+            <i
+              onClick={() => this.toggleDropdown(false)}
+              className="fa fa-filter"
+            />
+          </div>
+          <div className={`${this.state.isDropdownOpen ? "show" : "hide"}`}>
+            {createTree(data, search, selectedState, this.onChange)}
+          </div>
+        </Dropdown>
+      </React.Fragment>
+    );
+  }
 }
 
 Multiselect.propTypes = {
-    data: PropTypes.array,
-    onChange: PropTypes.func
+  data: PropTypes.array,
+  onChange: PropTypes.func
 };
 
 Multiselect.defaultProps = {
-    data: [],
-    onChange: () => { }
+  data: [],
+  onChange: () => {}
 };
 
 export default Multiselect;
--- src/Lib/TagInput.js
+++ src/Lib/TagInput.js
@@ -1,5 +1,6 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
+import PropTypes from "prop-types";
 import { size } from "lodash";
 
 const CustomInput = styled.label`
@@ -35,70 +36,95 @@ const CustomInput = styled.label`
 `;
 
 const findSelectedItems = selected => {
-    const newSelectedItems = [];
+  const newSelectedItems = [];
 
-    for (let key in selected) {
-        if (
-            selected[key].status === "true" &&
-            !selected[key].hasOwnProperty("child")
-        ) {
-            newSelectedItems.push({
-                id: key,
-                ...selected[key]
-            });
-        }
+  for (let key in selected) {
+    if (
+      selected[key].status === "true" &&
+      !selected[key].hasOwnProperty("child")
+    ) {
+      newSelectedItems.push({
+        id: key,
+        ...selected[key]
+      });
     }
-    return newSelectedItems;
+  }
+  return newSelectedItems;
 };
 
 class TagInput extends React.Component {
-    state = {
-        items: [],
-        focused: false,
-        input: ""
-    };
+  state = {
+    items: [],
+    focused: false,
+    input: ""
+  };
 
-    componentDidUpdate(prevProps) {
-        // Typical usage (don't forget to compare props):
-        const newSelectedItems = findSelectedItems(this.props.selected);
-        if (
-            this.props.selected &&
-            size(newSelectedItems) !== size(this.state.items)
-        ) {
-            this.setState({
-                items: newSelectedItems
-            });
-        }
+  componentDidUpdate(prevProps) {
+    // Typical usage (don't forget to compare props):
+    const newSelectedItems = findSelectedItems(this.props.selected);
+    if (
+      this.props.selected &&
+      size(newSelectedItems) !== size(this.state.items)
+    ) {
+      this.setState({
+        items: newSelectedItems
+      });
     }
+  }
 
-    render() {
-        return (
-            <CustomInput>
-                <ul>
-                    {this.state.items.map((item, i) => (
-                        <li key={i} onClick={this.handleRemoveItem(item)}>
-                            {item.title}
-                            <span>
-                                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAA3QAAAN0BcFOiBwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAECSURBVCiRnZHNTQNBDIU/e/YegfhpAG5EIi1EoaEkym4Jq4SZA1QBJSCxHYCEOPLTACFRckSZGS47GxZxAN7N9rOfnw3/RYxRyrLsfM+XZdmJMUqKNZGttZcmy16stb1UtNb2TJa9njt3kZokTTFZ9gzsAYsYwpmqSoQbYBeY+83mqCiKVSM1nU5PRfW2Jizr9A6wUJH+aDR6aBQSnHNdH0JVKwHMjWp/OBw+tjwkeO8jEJpDgP/4ErcanHNdoAIO6pWWAocmhGo2m50k3ta0MU+I7P9oOsY37/1xURQrBcjzfC0i18C7igwmk8n9eDy+U5EBsED1Ks/zdetBv33cn/EJBQJxcfY0zVIAAAAASUVORK5CYII=" />
-                            </span>
-                        </li>
-                    ))}
-                    <input value={this.state.input} onChange={this.handleInputChange} onKeyDown={this.handleInputKeyDown} />
-                </ul>
-            </CustomInput>
-        );
-    }
+  render() {
+    const { items, input } = this.state;
+    const itemsLength = items.length;
+    const MAX_NUMBER_OF_ELEMENTS = itemsLength > 3 ? 3 : itemsLength;
+    return (
+      <CustomInput>
+        <ul>
+          {items
+            .reverse()
+            .slice(0, MAX_NUMBER_OF_ELEMENTS)
+            .map((item, i) => (
+              <li key={i} onClick={this.handleRemoveItem(item)}>
+                {item.title}
+                <span>
+                  <img
+                    alt="Icon"
+                    src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAA3QAAAN0BcFOiBwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAECSURBVCiRnZHNTQNBDIU/e/YegfhpAG5EIi1EoaEkym4Jq4SZA1QBJSCxHYCEOPLTACFRckSZGS47GxZxAN7N9rOfnw3/RYxRyrLsfM+XZdmJMUqKNZGttZcmy16stb1UtNb2TJa9njt3kZokTTFZ9gzsAYsYwpmqSoQbYBeY+83mqCiKVSM1nU5PRfW2Jizr9A6wUJH+aDR6aBQSnHNdH0JVKwHMjWp/OBw+tjwkeO8jEJpDgP/4ErcanHNdoAIO6pWWAocmhGo2m50k3ta0MU+I7P9oOsY37/1xURQrBcjzfC0i18C7igwmk8n9eDy+U5EBsED1Ks/zdetBv33cn/EJBQJxcfY0zVIAAAAASUVORK5CYII="
+                  />
+                </span>
+              </li>
+            ))}
+          {itemsLength > MAX_NUMBER_OF_ELEMENTS && <li key="...">...</li>}
+          <input
+            onFocus={() => {
+              this.props.toggleDropdown(true);
+            }}
+            value={input}
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleInputKeyDown}
+          />
+        </ul>
+      </CustomInput>
+    );
+  }
 
-    handleInputChange = evt => {
-        this.setState({ input: evt.target.value });
-        this.props.onChange(evt);
+  handleInputChange = evt => {
+    this.setState({ input: evt.target.value });
+    this.props.onChange(evt);
+  };
+
+  handleRemoveItem = item => {
+    return () => {
+      this.props.onItemRemove(item);
     };
-
-    handleRemoveItem = item => {
-        return () => {
-            this.props.onItemRemove(item);
-        };
-    };
+  };
 }
+
+TagInput.propTypes = {
+  toggleDropdown: PropTypes.func
+};
+
+TagInput.defaultProps = {
+  toggleDropdown: () => {}
+};
 
 export default TagInput;
